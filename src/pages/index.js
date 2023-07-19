3c847fd0c5d8598ea228395770e4cace8445a946
Danilo Woo Hyung Kim <danilowoohyungkim@Danilos-MacBook-Pro-2.local>
2023-07-19 13:30:24 +0200

    first commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,118 +1,261 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import { useEffect, useState } from 'react';
+import 'tailwindcss/tailwind.css';
 
-const inter = Inter({ subsets: ['latin'] })
+class GameLogic {
+  constructor() {
+    this.board = this.emptyBoard();
+    this.score = 0;
+    this.addPiece();
+    this.addPiece();
+  }
+
+  emptyBoard() {
+    return Array(4).fill().map(() => Array(4).fill(0));
+  }
+
+  addPiece() {
+    let options = [];
+    this.board.forEach((row, i) => {
+      row.forEach((cell, j) => {
+        if (cell === 0) {
+          options.push({i, j});
+        }
+      });
+    });
+
+    if (options.length > 0) {
+      let spot = options[Math.floor(Math.random() * options.length)];
+      let r = Math.random();
+      this.board[spot.i][spot.j] = r > 0.1 ? 2 : 4;
+    }
+  }
+
+  slide(row) {
+    let arr = row.filter(val => val);
+    let missing = 4 - arr.length;
+    let zeros = Array(missing).fill(0);
+    arr = zeros.concat(arr);
+    return arr;
+  }
+
+  combine(row) {
+    for (let i = 3; i >= 1; i--) {
+      let a = row[i];
+      let b = row[i - 1];
+      if (a === b) {
+        row[i] = a + b;
+        this.score += row[i];
+        row[i - 1] = 0;
+      }
+    }
+    return row;
+  }
+
+  operate(row) {
+    row = this.slide(row);
+    row = this.combine(row);
+    row = this.slide(row);
+    return row;
+  }
+
+  rotateRight(matrix) {
+    let result = this.emptyBoard();
+    for (let i = 0; i < matrix.length; i++) {
+      for (let j = 0; j < matrix[i].length; j++) {
+        result[i][j] = matrix[j][i];
+      }
+    }
+    return result;
+  }
+
+  flip(matrix) {
+    return matrix.map(row => row.reverse());
+  }
+
+  moveLeft() {
+    let oldData = this.board.map(row => [...row]);
+    for (let i = 0; i < 4; i++) {
+      this.board[i] = this.operate(this.board[i]);
+    }
+    if (JSON.stringify(oldData) !== JSON.stringify(this.board)) {
+      this.addPiece();
+    }
+  }
+
+  moveRight() {
+    let oldData = this.board.map(row => [...row]);
+    this.board = this.flip(this.board);
+    for (let i = 0; i < 4; i++) {
+      this.board[i] = this.operate(this.board[i]);
+    }
+    this.board = this.flip(this.board);
+    if (JSON.stringify(oldData) !== JSON.stringify(this.board)) {
+      this.addPiece();
+    }
+  }
+
+  moveUp() {
+    let oldData = this.board.map(row => [...row]);
+    this.board = this.rotateRight(this.board);
+    for (let i = 0; i < 4; i++) {
+      this.board[i] = this.operate(this.board[i]);
+    }
+    this.board = this.rotateRight(this.board);
+    this.board = this.rotateRight(this.board);
+    this.board = this.rotateRight(this.board);
+    if (JSON.stringify(oldData) !== JSON.stringify(this.board)) {
+      this.addPiece();
+    }
+  }
+
+  moveDown() {
+    let oldData = this.board.map(row => [...row]);
+    this.board = this.rotateRight(this.board);
+    this.board = this.rotateRight(this.board);
+    this.board = this.rotateRight(this.board);
+    for (let i = 0; i < 4; i++) {
+      this.board[i] = this.operate(this.board[i]);
+    }
+    this.board = this.rotateRight(this.board);
+    this.board = this.rotateRight(this.board);
+    this.board = this.rotateRight(this.board);
+    if (JSON.stringify(oldData) !== JSON.stringify(this.board)) {
+      this.addPiece();
+    }
+  }
+
+  getBoard() {
+    return this.board;
+  }
+
+  getScore() {
+    return this.score;
+  }
+}
 
 export default function Home() {
+  const [game, setGame] = useState(null);
+  const [board, setBoard] = useState([]);
+  const [score, setScore] = useState(0);
+  const [bestScore, setBestScore] = useState(null);
+  const [hasMounted, setHasMounted] = useState(false);
+
+  useEffect(() => {
+    setHasMounted(true);
+    setBestScore(localStorage.getItem('bestScore') || 0);
+    const newGame = new GameLogic();
+    setGame(newGame);
+    setBoard(newGame.getBoard());
+    setScore(newGame.getScore());
+  }, []);
+
+  const handleKeyDown = (event) => {
+    if (!game) {
+      return;
+    }
+  
+    let newBoard;
+    switch (event.key) {
+      case 'd':
+      case 'ArrowRight':
+
+        game.moveLeft();
+        newBoard = game.getBoard().map(row => [...row]);
+        break;
+      case 's':
+      case 'ArrowDown':
+
+        game.moveUp();
+        newBoard = game.getBoard().map(row => [...row]);
+        break;
+      case 'w':
+      case 'ArrowUp':
+
+        game.moveDown();
+        newBoard = game.getBoard().map(row => [...row]);
+        break;
+      case 'a':
+      case 'ArrowLeft':
+
+        game.moveRight();
+        newBoard = game.getBoard().map(row => [...row]);
+        break;
+      default:
+        return;
+    }
+  
+    setBoard(newBoard);
+    setScore(game.getScore());
+    if (game.getScore() > bestScore) {
+      setBestScore(game.getScore());
+      localStorage.setItem('bestScore', game.getScore());
+    }
+  };
+
+
+  useEffect(() => {
+    window.addEventListener('keydown', handleKeyDown);
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [game, bestScore]);
+
+  const handleNewGame = () => {
+    // This should reset the game.
+    const newGame = new GameLogic();
+    setGame(newGame);
+    setBoard(newGame.getBoard());
+    setScore(newGame.getScore());
+  };
+
+  if (!hasMounted) {
+    return null;
+  }
+
+  function getColor(value) {
+    switch (value) {
+      case 2:
+        return 'bg-yellow-300';
+      case 4:
+        return 'bg-yellow-400';
+      case 8:
+        return 'bg-yellow-500';
+      case 16:
+        return 'bg-yellow-600';
+      case 32:
+        return 'bg-yellow-700';
+      case 64:
+        return 'bg-yellow-800';
+      case 128:
+        return 'bg-yellow-900';
+      case 256:
+        return 'bg-red-500';
+      default:
+        return 'bg-gray-200';
+    }
+  }
+
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <div className="bg-gray-900 text-white min-h-screen py-10 px-4 flex flex-col items-center justify-center">
+      <h1 className="text-6xl font-bold mb-8 text-yellow-300">Dragon Ball Z 2048</h1>
+      <div className="mb-4">
+        <button className="bg-yellow-500 hover:bg-yellow-600 text-black font-bold py-2 px-4 rounded" onClick={handleNewGame}>
+          New Game
+        </button>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className="grid grid-cols-4 gap-4 mb-4">
+        {board.map((row, i) => (
+          row.map((cell, j) => (
+            <div className={`h-20 w-20 flex items-center justify-center rounded-lg ${cell === 0 ? 'bg-gray-700' : getColor(cell) + ' text-black text-2xl font-bold'}`} key={i * 4 + j}>
+              {cell !== 0 && cell}
+            </div>
+          ))
+        ))}
       </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div>
+        <h2 className="text-3xl font-bold">Score: {score}</h2>
+        <h2 className="text-3xl font-bold">Best Score: {bestScore}</h2>
       </div>
-    </main>
+    </div>
   )
 }
